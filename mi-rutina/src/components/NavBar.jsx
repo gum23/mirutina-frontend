14618ac862c5efcad34f39a07201df8a0e42a3d5
Eduardo Unrrein <gum23coder@gmail.com>
2024-08-02 14:50:14 -0300

    solucion de problema con bootstrap

--- mi-rutina/src/components/NavBar.jsx
+++ mi-rutina/src/components/NavBar.jsx
@@ -5,13 +5,19 @@ import Navbar from 'react-bootstrap/Navbar';
 export const NavBar = () => {
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar style={{backgroundColor: "#3A4B5C"}} data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <div>
+            <img src="public/logo.svg" alt="logo" />
+          </div>
+          <Navbar.Brand >
+            <Nav.Link style={{fontFamily: "Holtwood One SC", fontSize: "32px"}} href='#home'>TU RUTINA</Nav.Link>
+          </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#inicio">INICIO</Nav.Link>
+            <Nav.Link href="#alumnos">ALUMNOS</Nav.Link>
+            <Nav.Link href="#perfil">PERFIL</Nav.Link>
+            <Nav.Link href="#ayuda">AYUDA</Nav.Link>
           </Nav>
         </Container>  
       </Navbar>
